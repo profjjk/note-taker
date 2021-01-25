605a5332d68d16b90fcdd205a59ccd4b087e1ffd
Jordan Kelly <profjjk@gmail.com>
2021-01-25 14:47:35 -0800

    test3

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ const app = express();
 // Listen for port, or default.
 const PORT = process.env.PORT || 3000;
 
+
+
 // Body parsers.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Route to html files.
+app.use(express.static('public'));
 
 
 // ROUTERS
 // ====================================================
 require("./routes/apiRoutes")(app); // Route to api.
-require("./routes/htmlRoutes")(app); // Route to html.
+require("./routes/htmlRoutes")(app); // Route to html.git checkout
 
 
 // LISTENER
