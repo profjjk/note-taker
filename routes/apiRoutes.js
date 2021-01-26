089cd1d33dacb7d6fc2d54409402d34c4b7cfd23
Jordan Kelly <profjjk@gmail.com>
2021-01-25 16:23:28 -0800

    remove single note GET

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,20 +11,6 @@ module.exports = function(app) {
   // GET ALL NOTES
   app.get("/api/notes", (req, res) => res.json(notes));
 
-  // GET SINGLE NOTE
-  app.get("/api/notes/:id", (req, res) => {
-    // Find note in array.
-    const found = notes.some(note => note.id === parseInt(req.params.id));
-    // IF note exists...
-    if (found) {
-      // Respond with note.
-      res.json(notes.filter(note => note.id === parseInt(req.params.id)));
-    } else {
-      // ELSE change status code and respond with error message.
-      res.status(400).json({ msg: "Note not found"});
-    }
-  });
-
   // ADD NEW NOTE
   app.post("/api/notes", (req, res) => {
     // Create a new note.
